5e79baf3360c5585f6ab4d6b6e6e984feaaf273e
Peh Zi Heng <pehziheng@gmail.com>
2021-06-23 20:58:54 +0800

    Add Landing Page

--- app/src/main/java/com/foundvio/setup/LoginFragment.kt
+++ app/src/main/java/com/foundvio/setup/LoginFragment.kt
@@ -1,10 +1,11 @@
-package com.foundvio
+package com.foundvio.setup
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.foundvio.R
 
 
 class LoginFragment : Fragment() {
